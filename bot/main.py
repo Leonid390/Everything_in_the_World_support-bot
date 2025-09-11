2377cfd2de86a520f5786259a1343366f10cc5ab
Leonid390 <leonidmashkov390@gmail.com>
2025-09-11 19:18:49 +0300

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -3,31 +3,25 @@ import telebot
 from logic import *
 bot = telebot.TeleBot(TOKEN)
 
-@bot.message_handler(commands=['help'])
+@bot.message_handler(commands=['help',  'start'])
 def handle_start(message):
-    bot.send_message(message.chat.id, "Я Бот поддержки интернет магазина 'Всё на свете'.")
-    bot.send_message(message.chat.id, "Спрашивайте ниже, что вам непонятно по заказам или темам, имеющим отношение к нашему магазину")
+    bot.send_message(message.chat.id, "Я Бот поддержки интернет магазина 'всё на свете'. Спрашивайте, что тебе не понятно по заказам или чему-то, имеющему отношение к нашему магазину")
+    bot.send_message(message.chat.id, "Спрашивайте ниже, что вам не понятно по заказам или темам, имеющим отношение к нашему магазину")
 
 @bot.message_handler(func=lambda m: True)
 def handle_message(message):
     question = message.text.strip()
-
-    conn = sqlite3.connect('qa_bot.db')
-    cursor = conn.cursor()
-    answer = cursor.execute("SELECT answer FROM qa WHERE question = ?", (question,)).fetchone()
-
+    
+    # Получаем ответ из logic.py
+    answer = q_and_a(question)
+    
+    # Такая же логика отправки сообщений
     if answer:
-        bot.send_message(message.chat.id, answer[0])
+        bot.send_message(message.chat.id, answer)
     else:
-        bot.send_message(message.chat.id, "Вопрос не имеет ответа, но добавлен в базу данных и может получить ответ через время. Попробуйте спросить позже")
-        cursor.execute("INSERT INTO qa (question, answer) VALUES (?, ?)", (question, ""))
-        conn.commit()
-
-    conn.close()
+        bot.send_message(message.chat.id, "Вопрос не имеет ответа, но добавлен в базу данных и может получить ответ позже. Попробуйте спроить позже")
 
 
 
 
 bot.infinity_polling()
-
-
